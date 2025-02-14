9ce49b85c89dff1a0666bd4d0b95024e37de18b7
Dina-DTS <dina20906@fci.bu.edu.eg>
2025-02-14 17:32:37 +0200

    all orders update

--- src/App.jsx
+++ src/App.jsx
@@ -20,8 +20,7 @@ import Wishlist from './Components/wishlist/wishlist'
 import ForgetPassword from './Components/User/ForgetPassword'
 import RestPassword from './Components/User/RestPassword'
 import CodeVerfiy from './Components/User/CodeVerfiy'
-import Allorders from './Components/allorders/allorders'
-
+import Orders from './Components/Orders/Orders'
 function App() {
   const [count, setCount] = useState(0)
 
@@ -40,7 +39,7 @@ function App() {
       {path:"forgetpass",element:<ProtectedAuth><ForgetPassword/></ProtectedAuth>},
       {path:"restpass",element:<ProtectedAuth><RestPassword/></ProtectedAuth>},
       {path:"codeverify",element:<ProtectedAuth><CodeVerfiy/></ProtectedAuth>},
-      {path:"allorders",element:<ProtectedAuth><Allorders/></ProtectedAuth>},
+      {path:"allorders",element:<ProtectedAuth><Orders/></ProtectedAuth>},
     ]}
   ])
 
--- src/Components/Orders/Orders.jsx
+++ src/Components/Orders/Orders.jsx
@@ -1,13 +1,13 @@
 import React, { useEffect } from 'react';
 import { useState } from 'react';
-import Style from "./Allorders.module.css/";
+import Style from "./Orders.module.css/";
 
-export default  function Allorders () {
+export default  function Orders () {
     const [string, setstring] = useState("hi");
     useEffect(()=>{},[])
     return (
         <div>
-            <h2 className={`${Style['bg-danger']}`}>all orders COMPO</h2>
+            <h2 className={`${Style['bg-danger']}`}>Orders COMPO</h2>
             <p>{string}Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facilis, iusto.</p>
         </div>
     );
